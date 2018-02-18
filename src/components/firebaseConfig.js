65fabe703a3891aeb1467db91fa94efff38515ef
Martin Forest <martin.forest@blueskytp.com>
2018-02-18 17:22:38 -0500

    Project creation. Firebase config and init added.

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyAITmfVx8yfk97btnPwAW_2Nw0zO20Ch1o",
+  authDomain: "vuejs-firestore-a4e5f.firebaseapp.com",
+  databaseURL: "https://vuejs-firestore-a4e5f.firebaseio.com",
+  projectId: "vuejs-firestore-a4e5f",
+  storageBucket: "vuejs-firestore-a4e5f.appspot.com",
+  messagingSenderId: "573833020186"
+}
